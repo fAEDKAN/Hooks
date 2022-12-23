e7156a31dec9e1f1c447c75d289c459be00ef486
fAEDKAN <sfdk.94@hotmail.com>
2022-12-23 16:32:59 -0300

    hooks

---
+++ src/components/Error404.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Error404() {
+    return (
+        <div className="container-fluid">
+    
+                <h1 className="h3 mb-0 text-gray-800">3rr0r 404 Q(o.oQ)</h1>
+                <p>Ésta ruta es inexistente o está en una piña debajo del mar.</p>
+                <img src="/images.jpg" alt="bob el ponja" />
+
+        </div>
+    )
+}
